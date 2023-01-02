2f49ac1a690c9ca783194c3f25c1fb2966d2650c
Dhay-10 <83820572+Dhay-10@users.noreply.github.com>
2023-01-02 22:27:30 +0000

    Added Changes to Admin area to increase usability

--- TricksterMain/admin.py
+++ TricksterMain/admin.py
@@ -3,7 +3,27 @@ from .models import SkillLevel
 from .models import Trick
 from .models import User
 
-admin.site.register(SkillLevel)
-admin.site.register(Trick)
-admin.site.register(User)
+#admin.site.register(SkillLevel)
+#admin.site.register(Trick)
+#admin.site.register(User)
 
+
+@admin.register(Trick)
+class TrickAdmin(admin.ModelAdmin):
+    fields = ('TrickName', 'TrickRecLevel', 'TrickDifficulty','TrickDiscription', 'TrickHowTo', 'TrickImg',)
+    list_display = ('TrickName', 'TrickRecLevel', 'TrickDifficulty',)
+    ordering = ('TrickDifficulty',)
+    search_fields = ('TrickName',)
+    
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('FirstName', 'LastName', 'Username','Email', 'SkillLevel', 'DateOfJoining', 'ProfilePhoto', 'LearnedTricks',)
+    list_display = ('LastName', 'FirstName', 'Username','SkillLevel', 'DateOfJoining',)
+    ordering = ('-DateOfJoining',)
+    search_fields = ('Username', 'UserID')
+
+@admin.register(SkillLevel)
+class SkillLevelAdmin(admin.ModelAdmin):
+    list_display = ('SkillLevelName', 'SkillLevelID',)
+    ordering = ('SkillLevelID',)
+    search_fields = ('SkillLevelName', 'SkillLevelID',)
